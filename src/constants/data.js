c3f4a93c5f89443f273b93539de4f860dfd93000
prabhav <prabhavdixit007@gmail.com>
2024-09-28 21:06:41 +0530

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,34 @@
+const filters = [
+  {
+    name: "All",
+    imageUrl:
+      "https://imgs.search.brave.com/Ke12yjbJhiqHLRWmTU4Dz_U0_ZCuv8HvCNL3BNqwPq0/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNy8w/OC8wOC8wMC8zMy9j/b25zdGVsbGF0aW9u/cy0yNjA5NjQ3XzY0/MC5qcGc",
+  },
+  {
+    name: "Food",
+    imageUrl:
+      "https://imgs.search.brave.com/LR7dsaYuSancU90gf7LIRbjhffWyxeM2NEcme-UXzIA/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9waXp6YS13aXRo/LXRvbWF0b2VzLWJh/c2lsLWJhc2lsLXdv/b2Rlbi10YWJsZV8x/MDQ0OTQzLTY0NjA5/LmpwZz9zaXplPTYy/NiZleHQ9anBn",
+  },
+  {
+    name: "Fitness",
+    imageUrl:
+      "https://imgs.search.brave.com/Xvv5uaZXzaBnfHs3KLe0nC70luHGQAjxpBMugfAzLcA/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9neW0tc3BvcnRz/LXJvb20tYXJ0aWZp/Y2lhbC1pbnRlbGxp/Z2VuY2UtZ2VuZXJh/dGl2ZV8xMzAyODMw/LTEyNy5qcGc_c2l6/ZT02MjYmZXh0PWpw/Zw",
+  },
+  {
+    name: "Travel",
+    imageUrl:
+      "https://imgs.search.brave.com/HZpVhZkBWttl1fd4svb7EdouwufnamZThQl_6FNdPzQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvODYw/NTI4NzU2L3Bob3Rv/L3RoZS1iYW5kcmF3/b3JsaS1zZWEtbGlu/ay1tdW1iYWktaW5k/aWEud2VicD9hPTEm/Yj0xJnM9NjEyeDYx/MiZ3PTAmaz0yMCZj/PWE1RHdzMXRLUU9o/Tk9vTE9LcFFVSEV1/VUViRVoyRFNXTkkz/SjlVQl9nZVU9",
+    },
+  {
+    name: "Movie",
+    imageUrl:
+      "https://imgs.search.brave.com/QV7Lsf0i1Kz4uW-eWfnNKY7vcq9VXizPNe7vIzLd3yE/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YTEudGhyaWxsb3Bo/aWxpYS5jb20vZmls/ZXN0b3JlL2hkaTNi/MTYwcGZ5OHQ3aTU4/NGl5OG5nZjg1YTJf/SW1nX1dvcmxkX2Rm/ZTRjOTAyM2MuanBn/P3c9MTA4MCZkcHI9/Mg",
+  },
+  {
+    name: "Education",
+    imageUrl:
+      "https://imgs.search.brave.com/xHLw_9LhVnLLzdbfosDsQJVY8V2hQyg6ZVej9W_xs8s/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/Y3JlYXRlLnZpc3Rh/LmNvbS9hcGkvbWVk/aWEvc21hbGwvMTY0/ODA2NjU2L3N0b2Nr/LXBob3RvLXNjaG9v/bC1zdXBwbGllcw.jpeg",
+  },
+];
+
+export default filters;
